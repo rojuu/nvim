05a34da598d77e7f09996cd7783f81cf7fa93b57
Roni Juppi <roju269@gmail.com>
2026-01-04 00:31:09 +0200

    Update the config with a new fresh theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -50,16 +50,38 @@ return {
       },
     },
   },
+
   {
-    'WTFox/jellybeans.nvim',
-    priority = 1000,
-    lazy = false,
+    'rebelot/kanagawa.nvim',
+    name = 'kanagawa',
     config = function()
-      require('jellybeans').setup {
-        transparent = true,
-        italics = true,
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = false },
+        statementStyle = { bold = false },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'dragon', -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+          dark = 'dragon', -- try "dragon" !
+          light = 'lotus',
+        },
       }
-      vim.cmd.colorscheme 'jellybeans-light'
+
+      -- setup must be called before loading
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
 
