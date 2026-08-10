94f1e8e442eeb39d1671af3ee2fd2d5cb6542df8
Roni Juppi <roju269@gmail.com>
2026-08-10 20:47:06 +0300

    Automatic enable Mason installed LSPs

--- init.lua
+++ init.lua
@@ -646,7 +646,7 @@ do
 
   -- Translates between nvim-lspconfig server names and mason.nvim package names (e.g. lua_ls <-> lua-language-server)
   require('mason-lspconfig').setup {
-    automatic_enable = false, -- Change this to true if you want to automatically enable servers that are installed manually (e.g. via :Mason / :MasonInstall)
+    automatic_enable = true, -- Change this to true if you want to automatically enable servers that are installed manually (e.g. via :Mason / :MasonInstall)
   }
 
   -- Ensure the servers and tools above are installed
