77e8b8d9dc067011180e96a9720c0cc24e655f0a
Roni Juppi <roju269@gmail.com>
2026-08-10 22:19:07 +0300

    Fix firebase rules syntax

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Roni's neovim config
 
 Currently tested with [NVIM v0.12.4](https://github.com/neovim/neovim/releases/tag/v0.12.4)
 
+## Firebase rules syntax
+
+You have to run `:TSUpdate rules` once for the firebase rules syntax to work
+
--- init.lua
+++ init.lua
@@ -431,20 +431,25 @@ do
     end,
   })
 
-  local treesitter_parsers = require 'nvim-treesitter.parsers'
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'TSUpdate',
+    callback = function()
+      -- Firestore rules
+      require('nvim-treesitter.parsers').rules = {
+        install_info = {
+          url = 'https://github.com/rojuu/tree-sitter-firebase-rules',
+          revision = '038f798fc68314696c59c571bcc022546c3bf790',
+          branch = 'main',
+          files = { 'src/parser.c' },
+          generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = true, -- could regenaret in the repo, but too lazy to do that. At least the grammar file works fine
+        },
+        filetype = 'rules', -- if filetype does not match the parser name
+      }
+    end,
+  })
 
-  -- Firestore rules
-  treesitter_parsers.rules = {
-    install_info = {
-      url = 'https://github.com/rojuu/tree-sitter-firebase-rules',
-      revision = '038f798fc68314696c59c571bcc022546c3bf790',
-      branch = 'main',
-      files = { 'src/parser.c' },
-      generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-      requires_generate_from_grammar = true, -- could regenaret in the repo, but too lazy to do that. At least the grammar file works fine
-    },
-    filetype = 'rules', -- if filetype does not match the parser name
-  }
+  vim.treesitter.language.register('rules', { 'rules' })
   -- TODO: Port this to lua when I'm not lazy
   -- For .rules files the ft was detected as "hog" for some reason
   vim.cmd [[au BufRead,BufNewFile *.rules set filetype=rules]]
